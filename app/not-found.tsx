1ed59e0ca40be47832e0dd84f3a152d23a522737
Evil's <evils@evils.cc>
2025-02-06 14:57:10 +0800

    <type>(<scope>): <subject> <BLANK LINE> <body> <BLANK LINE> <footer>

---
+++ app/not-found.tsx
@@ -0,0 +1,22 @@
+"use client";
+import { Suspense } from "react";
+
+// Error boundaries must be Client Components
+
+export default function GlobalError({
+  error,
+  reset,
+}: {
+  error: Error & { digest?: string };
+  reset: () => void;
+}) {
+  return (
+    // eslint-disable-next-line jsx-a11y/html-has-lang
+    <html>
+      <body>
+        <h2>Something went wrong!</h2>
+        <button onClick={() => reset()}>Try again</button>
+      </body>
+    </html>
+  );
+}
